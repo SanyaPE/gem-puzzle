483dca85156b86265c54fcf96655a536993092c2
Sanya84 <pepleralex@gmail.com>
2022-10-20 17:05:55 +0300

    fix: script change set properties

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@ const gamePuzzle = {
     fieldTarget: [],
     fieldCurr: null,
     fieldSize: 4,
+    null: null,
+    available: [],
     classes: ["container", "main", "field", "field__item", "empty"],
   },
-  // 1
+
+  //Запуск кода
+  //создаем рабочий 2мерный массив _createFieldTarget и настройки Null
   init() {
     console.log("start init");
     this._createFieldTarget();
   },
-  // 2
+
+  //Создаем базовый массив согласно fieldSize
+  //вносим в properties массив для сравнения результвта
   _createFieldTarget() {
     let fieldSize = this.properties.fieldSize;
     for (let i = 0; i < Math.pow(fieldSize, 2); i++) {
-    //   console.log(i);
+      //   console.log(i);
       this.properties.fieldTarget[i] =
         i < Math.pow(fieldSize, 2) - 1 ? i + 1 : null;
     }
     // console.log(this.properties.fieldTarget);
     this._createFieldCurrent();
   },
-  // 3
+
+  // Перемешиваем базовый массив (_shuffleArray)
+  // создаем рабочий 2мерный массив,
+  // проверка решаемости _isSolved, false - запуск заново
+  // true - Null add to properties (_setNulltoProp)
   _createFieldCurrent() {
     let array = [...this.properties.fieldTarget];
     let fieldCurr = [];
@@ -30,7 +40,7 @@ const gamePuzzle = {
     array = this._shuffleArray(array);
     // console.log(array);
     for (let i = 0; i < array.length; ) {
-    //   console.log(i);
+      // console.log(i);
       fieldCurr.push(array.slice(i, i + this.properties.fieldSize));
       i += 4;
     }
@@ -39,8 +49,10 @@ const gamePuzzle = {
     // console.log(isSolved);
     if (isSolved) this.properties.fieldCurr = fieldCurr;
     else this._createFieldCurrent();
+    this._setNulltoProp();
   },
-  //4
+
+  //Перемешиваем базовый массив
   _shuffleArray(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -48,6 +60,7 @@ const gamePuzzle = {
     }
     return arr;
   },
+
   setLocalStorage(setGame) {
     storageGame.push(setGame);
     storageGame.sort((a, b) => a.move - b.move);
@@ -55,12 +68,15 @@ const gamePuzzle = {
     localStorage.setItem("previousGame", JSON.stringify(storageGame));
     return storageGame;
   },
+
+  // проверка fieldCurr с готовым решением fieldTarget
   isMatchTarget() {
     let target = this.properties.fieldTarget.flat();
     let current = this.properties.fieldCurr.flat();
     return target.every((el, i) => el == current[i]);
   },
-  //5
+
+  //Проверка на решаемость раздачи
   _isSolved(FieldCurrent) {
     let temp = FieldCurrent;
     // console.log(temp);
@@ -69,7 +85,7 @@ const gamePuzzle = {
       for (let j = 0; j < temp[i].length - 1; j++) {
         // console.log(temp[i][j]);
         if (temp[i][j]) {
-        //   console.log(temp[i][j], temp[i].slice(j + 1));
+          //   console.log(temp[i][j], temp[i].slice(j + 1));
           temp[i]
             .slice(j + 1)
             .forEach((elem) => (elem < temp[i][j] && elem ? count++ : null));
@@ -80,25 +96,50 @@ const gamePuzzle = {
     // console.log(count);
     return count % 2 ? false : true;
   },
-  toMove() {},
-  createElements(){
 
+  //Настройки Null в properties
+  _setNulltoProp() {
+    this.properties.null = this._searchElement(null);
+    // координаты доступных для перестановки элементов
+    let propNull = this.properties.null;
+    let propAval = this.properties.available;
+    let propSize = this.properties.fieldSize;
+    propAval[0] = propNull[0] > 0 ? [propNull[0] - 1, propNull[1]] : null;
+    propAval[1] = propNull[1] > 0 ? [propNull[0], propNull[1] - 1] : null;
+    propAval[2] =
+      propNull[1] < propSize - 1 ? [propNull[0], propNull[1] + 1] : null;
+    propAval[3] =
+      propNull[0] < propSize - 1 ? [propNull[0] + 1, propNull[1]] : null;
+  },
 
-  }
+  toMove() {
+    if (event.target.closest(".field__item")) {
+      console.log(event.target);
+      let elemClick = gamePuzzle._searchElement(event.target.innerHTML);
+      console.log("elemClick =>", elemClick);
+    }
+  },
+
+  _searchElement(element) {
+    for (i in this.properties.fieldCurr) {
+      for (j in this.properties.fieldCurr[i]) {
+        if (this.properties.fieldCurr[i][j] == element) element = [+i, +j];
+      }
+    }
+    return element;
+  },
+
+  createElements() {},
 };
 
 gamePuzzle.init();
-console.log(gamePuzzle.properties.fieldTarget);
+// console.log(gamePuzzle.properties.fieldTarget);
 console.log(gamePuzzle.properties.fieldCurr);
-const tik = document.querySelector(".tik")
-tik.addEventListener("click", move)
-function move(){
-  console.log(event.target);
-  console.log(event.target.innerHTML);
-  console.log(tik.innerHTML);
-  // let a = tik.style.transform = 'translateY(105%)'
-  let a = tik.style.transform = 'translateX(-105%)'
-  console.log(a);
-}
+console.log(gamePuzzle.properties.null);
+console.log(gamePuzzle.properties.available);
+// console.log(gamePuzzle.properties.available[0]);
+// let a = tik.style.transform = 'translateY(105%)'
 
 //element.style.transform: 'translateY(-n%)'
+const field = document.querySelector(".field");
+field.addEventListener("click", gamePuzzle.toMove);
